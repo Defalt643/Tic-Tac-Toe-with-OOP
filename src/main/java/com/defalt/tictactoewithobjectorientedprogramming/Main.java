415c8556dd66559f581646fda22eee20e05c27fb
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-07-17 15:10:36 +0700

    Remove unnecessary code.

--- src/main/java/com/defalt/tictactoewithobjectorientedprogramming/Main.java
+++ src/main/java/com/defalt/tictactoewithobjectorientedprogramming/Main.java
@@ -1,17 +1,15 @@
 package com.defalt.tictactoewithobjectorientedprogramming;
 
-import java.util.*;
-
 public class Main {
+
     public static void main(String[] args) {
-        Game game=new Game();
-        while(true){
+        Game game = new Game();
+        while (true) {
             game.startGame();
-            if(!game.restartGame()){
+            if (!game.restartGame()) {
                 break;
             }
         }
-        
 
     }
 }
